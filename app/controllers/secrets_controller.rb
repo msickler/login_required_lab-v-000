6113d34b79d34bee284d710723835a8092c98e2e
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-14 00:02:00 +0000

    automatically backed up by learn

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -2,7 +2,11 @@ class SecretsController < ApplicationController
   before_action :require_login
 
   def show
+    if !current_user
+      redirect_to '/sessions/login'
+     end
   end
+  
 
 
 end
